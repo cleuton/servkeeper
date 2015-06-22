47fe7c5d0cf5be5847c9456b40832237ba165b52
cleuton <cleuton.sampaio@gmail.com>
2015-06-22 09:59:01 -0300

    Correção da classe RestResource

---
+++ servkeeper/src/main/java/com/obomprogramador/microservice/servkeeper/model/RestResponse.java
@@ -0,0 +1,9 @@
+package com.obomprogramador.microservice.servkeeper.model;
+
+public class RestResponse {
+	public class Data {
+		public String mensagem;
+	}
+	public String status;
+	public Data data;
+}

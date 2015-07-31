6a9563389c85dcbc648ce432e73ae13b56c6c400
cleuton <cleuton.sampaio@gmail.com>
2015-07-31 15:27:12 -0300

    Increment fix

--- ServiceClient/src/main/java/com/obomprogramador/microservice/servkeeper/ServiceClient/Increment.java
+++ ServiceClient/src/main/java/com/obomprogramador/microservice/servkeeper/ServiceClient/Increment.java
@@ -6,11 +6,13 @@ import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.atomic.DistributedAtomicLong;
 import org.apache.curator.retry.RetryNTimes;
 import org.apache.curator.retry.RetryUntilElapsed;
+import org.apache.log4j.Logger;
 
 public class Increment {
 	private CuratorFramework client;
 	private String counterPath;
 	private DistributedAtomicLong counter;
+	private Logger logger = Logger.getLogger(this.getClass());
 	
 	public Increment(String zkAddress, String counterPath) {
 		this.client = 
@@ -22,12 +24,24 @@ public class Increment {
 	public void increment() throws Exception {
 		int tempoMaximoDeTentativasMilissegundos = 1000;
 		int intervaloEntreTentativasMilissegundos = 100;
-		RetryPolicy rp = new RetryUntilElapsed(tempoMaximoDeTentativasMilissegundos, 
-				intervaloEntreTentativasMilissegundos);
-		this.counter = new DistributedAtomicLong(this.client,
-                this.counterPath,
-                rp);
-		this.counter.increment();
+		try {
+			RetryPolicy rp = new RetryUntilElapsed(tempoMaximoDeTentativasMilissegundos, 
+					intervaloEntreTentativasMilissegundos);
+			this.counter = new DistributedAtomicLong(this.client,
+	                this.counterPath,
+	                rp);
+			logger.debug("## INCREMENT WILL BEGIN");
+			if (this.counter.get().succeeded()) {
+				logger.debug("## INCREMENT GET COUNTER (BEFORE): " + this.counter.get().postValue());
+				if(this.counter.increment().succeeded()) {
+					logger.debug("## INCREMENT COUNTER AFTER: " + this.counter.get().postValue());
+				}
+			}
+			this.counter.increment();			
+		}
+		catch(Exception ex) {
+			logger.error("********* INCREMENT COUNTER ERROR: " + ex.getMessage());
+		}
 
 	}
 }
